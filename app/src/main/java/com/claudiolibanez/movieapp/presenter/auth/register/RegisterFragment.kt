ba03be7ef15e5dcb7ab099881ad9d1c685b9fc6a
Claudio Soares <claudiolibanez@gmail.com>
2023-12-19 22:38:12 -0300

    Configurando dependências do Firebase

--- app/src/main/java/com/claudiolibanez/movieapp/presenter/auth/register/RegisterFragment.kt
+++ app/src/main/java/com/claudiolibanez/movieapp/presenter/auth/register/RegisterFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.claudiolibanez.movieapp.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class RegisterFragment : Fragment() {
 
     override fun onCreateView(
